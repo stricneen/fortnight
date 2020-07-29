86f29840c37ce29d58d17969c48d6b9372abeba0
stricneen <stricneen@gmail.com>
2020-07-29 16:01:06 +0100

    Add files via upload

---
+++ C#/Pair programming Csharp/fortnitewinner/UnitTestsFortniteWinner/TestResultsLineParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using fortnitewinner.helpers;
+using fortnitewinner.interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsFortniteWinner
+{
+    [TestClass]
+    public class TestResultsLineParser
+    {
+        private static readonly string EXAMPLE_RESULTS_LINE =
+            "AlsAllStars                  4                2               5";
+
+        private ResultsLineParser parser;
+
+        public TestResultsLineParser()
+        {
+            parser = new ResultsLineParser(EXAMPLE_RESULTS_LINE);
+        }
+
+        [TestMethod]
+        public void ExtractsTeamName()
+        {
+            Assert.AreEqual("AlsAllStars", parser.GetTeamName());
+        }
+
+    }
+}
